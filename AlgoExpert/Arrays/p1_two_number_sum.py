f3953274e80572cde0564b26e4297ffd33b05685
ADITYAS1000 <adijj332@gmail.com>
2023-01-19 08:56:56 -0500

    My commit

--- AlgoExpert/Arrays/p1_two_number_sum.py
+++ AlgoExpert/Arrays/p1_two_number_sum.py
@@ -3,52 +3,75 @@
 # Avoid O(n^2)
 # Print pair of two numbers which sum upto target
 
-# array sorted?
-# are negatives allowed?
-# can array be empty?
+# array sorted? : No, but you can
+# are negatives allowed? : Yes
+# can array be empty? : No
+
+# Approach 1: (Iterative) O(n^2) | O(1)
+# def SumOfTwo(arr, target):
+#     for i in range(len(arr)):
+#         for j in range(i+1,len(arr)):
+#             if arr[i] + arr[j] == target:
+#                 return [arr[i] , arr[j]]
 
 # Two pointer approach [1,2,3,4,5] T = 6 : i,j .. i<j if i+j == target save.. i++ j-- .. (i + j) < target i++ (i + j) > target j--
 
-# O(nlog(n)) T | O(1) S
-# def SumOfTwo(lst, target):
+# O(n) | O(1)
+# def SumOfTwo(arr, target):
+#     arr.sort()
 #     result = []
-#     i, j = 0, len(lst) - 1
 
-#     while(i < j):
-#         if((lst[i] + lst[j]) == target):
-#             result.append((lst[i], lst[j]))
-#             i+=1
-#             j-=1
-#         elif((lst[i] + lst[j]) < target):
-#             i += 1
+#     l,r = 0, len(arr) - 1
+
+#     while l < r:
+#         if (arr[l] + arr[r]) > target:
+#             r = r - 1
+#         elif (arr[l] + arr[r]) < target:
+#             l = l + 1
 #         else:
-#             j -= 1
+#             result.append([arr[l],arr[r]])
+#             l, r = l+1, r-1
 #     return result
 
-# # lst = [5,4,3,2,1] #   In 2 pointer approach, sorting of list does not matter.. the order of pair is changed depending if input if descending or ascending
-# #                       For random ordered input, pairs are based on insertion order
-# # target = 6
-# lst = [3, 5, -4, 8,11,1,-1,6]
+# lst = [5,3,4,1,2] #   In 2 pointer approach, sorting of list DOES matter.. gives only [5,1]
+# lst = [1,2,3,4,5] #   gives both [1,5] and [2,4]
+# target = 6
+# lst = [-4, 5, 11, 8,3,1,-1,6]
 # target = 10
 
+# Appraoch 3: Hashmap
+# O(n) T | O(n) S
+def SumOfTwo(arr, target):
+    hashmap = {}
+    result = []
+    for element in arr:
+        complement = target - element
+        if complement in hashmap:
+            print(hashmap)
+            result.append([complement,element])
+            del hashmap[complement]
+        else:
+            hashmap[element] = True
+    return result
 
 # O(n) T | O(n) S
 # Complement approach
-def SumOfTwo(lst, target):
-    hashmap = {}
-    complement = 0
-    result = []
-    for index in range(0,len(lst)):
-        element = lst[index]
-        complement = target - element
-        if(element in hashmap):
-            result.append((element, complement))
-            del hashmap[element]
-        else:
-            hashmap[complement] = index
-    return result
+# def SumOfTwo(lst, target):
+#     hashmap = {}
+#     complement = 0
+#     result = []
+#     for index in range(0,len(lst)):
+#         element = lst[index]
+#         complement = target - element
+#         if(element in hashmap):
+#             result.append((element, complement))
+#             del hashmap[element]
+#         else:
+#             hashmap[complement] = index
+#     return result
 
-lst = [3, 5, -4,-1, 8,11,1,11,-1,6] # there are 2 pairs : 11,-1...11,-1 . 2 outputs .. 11,-1 , 11 : 1 pair since for 2nd time, -1 got matched and didn't create entry in dict
+lst = [3, 5, -4,11, 8,-1,1,11,-1,6] # there are 2 pairs : 11,-1...11,-1 . 2 outputs .. 11,-1 , 11,-1
+lst = [3, 5, -4,11, 8,-1,1,11,6]    # 1 output .. 11,-1 since on getting pair, we delete hashmap entry meaning there can be another output only if there is another 11 and -1 in input
 target = 10
 
 print(SumOfTwo(lst, target))
--- AlgoExpert/Arrays/p2_validate_subsequence.py
+++ AlgoExpert/Arrays/p2_validate_subsequence.py
@@ -2,23 +2,26 @@ lst_input = [5,1,22,25,6,-1,8,10]
 lst_check = [1,6,-1,10]
 
 
-# T : O(m * log(n+m)) | S: O(1)
-# def ValidateSubsequence(lst_input, lst_check):
-#     for index in range(0,len(lst_check)):
-#         print(lst_input[index], lst_check[index])
-#         if(lst_input[index] != lst_check[index]):
-#             return False
-#     return True 
+# O(n) | O(1) : For loop solution
+# def ValidateSubsequence(array, sequence):
+#     seqIdx = 0
+#     for value in array:
+#         if seqIdx == len(sequence):
+#             break
+#         if value == sequence[seqIdx]:
+#             seqIdx += 1
+#     return seqIdx == len(sequence)
 
-# T : O(n) | S : O(1)
-def ValidateSubsequence(lst_input, lst_check):
-    seqIdx = 0
-    for value in lst_input:
-        if(seqIdx == len(lst_check)):
-            break
-        if(value == lst_check[seqIdx]):
+
+# T : O(n) | S : O(1) : While loop solution
+def ValidateSubsequence(array, sequence):
+    seqIdx, arrayIdx = 0, 0
+    while seqIdx < len(sequence) and arrayIdx < len(array):
+        if sequence[seqIdx] == array[arrayIdx]:
             seqIdx += 1
-            print(value, seqIdx)
-    return seqIdx == len(lst_check)
+            arrayIdx += 1
+        else:
+            arrayIdx += 1
+    return seqIdx == len(sequence)
 
 print(ValidateSubsequence(lst_input, lst_check))
